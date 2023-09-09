2961a3d7900d6e3f1dda60de4cc529f0f823e886
TruptimayeePanigrahy <panigrahydeepakkumar27@gmail.com>
2023-09-09 08:10:14 +0530

    update schema

--- Backend/models/userpost.model.js
+++ Backend/models/userpost.model.js
@@ -9,6 +9,10 @@ const userpostschema = mongoose.Schema({
     type: [String], // Store user IDs who liked the post
     default: [],    // Initialize as an empty array
   },
+   likesCount: {
+    type: Number,
+    default: 0,     // Initialize the like count to 0
+  },
 }, {
   versionKey: false,
   timestamps: true
